b375129fdffe904a1a77d8839a2d8ebcf66086c8
Akash suri <akashsuri1979@gmail.com>
2024-12-29 17:24:11 +0530

    Updated dark button
    made some changes in the components

--- frontend/src/components/darkButton.js
+++ frontend/src/components/darkButton.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { FaSun, FaMoon } from 'react-icons/fa'; // Importing icons from react-icons
-
+import { FaSun, FaMoon } from "react-icons/fa";
 
 export function darkButton({ isDarkMode, toggleDarkMode }) {
   return (
-      <>
+    <>
       <button
         onClick={toggleDarkMode}
-        className={`ml-4 p-2 rounded-full flex items-center justify-center ${isDarkMode ? 'bg-gray-700' : 'bg-black'} text-white transition duration-300 hover:bg-gray-600`}
+        className={`w-12 h-12 rounded-full flex items-center justify-center ${
+          isDarkMode ? "bg-custom-green" : "bg-custom-green"
+        } text-white transition duration-300 hover:bg-custom-grey-color`}
       >
         {isDarkMode ? <FaSun size={24} /> : <FaMoon size={24} />}
       </button>
-      </>
+    </>
   );
 }
