679b1c3311e51363f0a3adaa07829c244b0f44bd
Akash suri <akashsuri1979@gmail.com>
2024-12-29 17:22:07 +0530

    Updated Frontend
    made some changes in app.js,index.css as well as some changes in the constants

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import Rankings from './pages/Ranking';
-import PublicProfile from './pages/PublicProfile';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage"; // Your existing landing page
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile/:username" element={<PublicProfile />} />
-        <Route path="/rankings" element={<Rankings />} />
-      </Routes>
+      <div>
+        <Routes>
+          {/* Landing Page */}
+          <Route path="/" element={<LandingPage />} />
+
+          {/* Authentication Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
