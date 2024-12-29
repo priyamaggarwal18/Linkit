49917280a67c0b6637e4b7d9c5d503d08b39adf9
Priyam Aggarwal <122437027+priyamaggarwal18@users.noreply.github.com>
2024-12-29 20:58:37 +0530

    Update copyLink.js

--- frontend/src/components/copyLink.js
+++ frontend/src/components/copyLink.js
@@ -1,22 +1,18 @@
 import React from "react";
 import { FaCopy } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
 
 export function CopyButton({ link }) {
-  const navigate = useNavigate();
-
-  const handleCopyAndNavigate = () => {
+  const handleCopy = () => {
     navigator.clipboard.writeText(link)
       .then(() => {
         alert("Link copied to clipboard!");
-        navigate(link);
       })
       .catch((err) => console.error("Failed to copy the link:", err));
   };
 
   return (
     <button
-      onClick={handleCopyAndNavigate}
+      onClick={handleCopy}
       className="w-12 h-12 rounded-full flex items-center justify-center bg-custom-green text-white transition duration-300 hover:bg-green-600"
     >
       <FaCopy size={24} />
